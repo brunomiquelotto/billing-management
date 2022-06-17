12d8ee52775baa2d339568d202e5b49ed112c57c
Silas S. Caxias <silascaxias@gmail.com>
2022-06-17 17:57:47 -0300

    Added bill.service requests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getBills }  from './services/bills.service';
+import { getBills, addBill, updateBill, deleteBill, copyBillFromLastMonth, payBill }  from './services/bills.service';
 import React, { useEffect, useState } from "react";
 
 function App() {
@@ -7,24 +7,89 @@ function App() {
 
   useEffect(() => {
     async function fetchBills () {
-      const result = await getBills({year: '2022', month: '07'});
+      const result = await getBills();
       console.log(result.data);
       setBills(result.data);
     }
     fetchBills();
   }, []);
   
+  async function newBill() {
+    const bill = {
+      description: 'descriçao teste',
+      group: 'grupo 2',
+      value: 10,
+      paymentDate: null,
+      dueDate: '2022-05-05',
+      isFixed: true,
+      obs: null
+    }
+    const result = await addBill(bill)
+    console.log(result);
+  }
+  
+  async function updateFirstBill() {
+    const billToUpdate = {
+      id: 1,
+      description: `descriçao atualizada ${Date()}`,
+      group: 'grupo 2',
+      value: 10,
+      paymentDate: null,
+      dueDate: '2022-05-05',
+      isFixed: true,
+      obs: null
+    }
+    const result = await updateBill(billToUpdate)
+    console.log(result);
+  }
+
+  async function removeBill() {
+    const billID = bills[0].id
+    if (billID != undefined) {
+      const result = await deleteBill(billID)
+      console.log(result);
+    }
+  } 
+
+  async function copyBill() {
+    const result = await copyBillFromLastMonth()
+    console.log(result);
+  } 
+
+  async function payFirstBill() {
+    const billID = bills[0].id
+    if (billID != undefined) {
+      const result = await payBill(billID)
+      console.log(result);
+    }
+  } 
+
   return (
     <div className="App">
       <table>
         <tbody>
             {bills.map((bill) =>
               <tr key={bill.id}>
-                <td>{bill.description}</td>
+                <td>Desc: {bill.description} Payment: {bill.paymentDate}</td>
               </tr>
             )}
         </tbody>
       </table>
+      <button onClick={newBill}>
+        New Bill
+      </button>
+      <button onClick={removeBill}>
+        Delete First Bill
+      </button>
+      <button onClick={updateFirstBill}>
+        Update First Bill
+      </button>
+      <button onClick={copyBill}>
+        Copy Bill From Last Month
+      </button>
+      <button onClick={payFirstBill}>
+        Pay Last Bill
+      </button>
     </div>
   );
 }
--- src/services/bills.service.js
+++ src/services/bills.service.js
@@ -1,7 +1,31 @@
 import API from './api';
   
-const getBills = async ({year, month}) => {
+const getBills = async () => {
+    return await API.get(`bills`);
+}
+
+const getBillsByDate = async ({year, month}) => {
     return await API.get(`bills/${year}/${month}`);
 }
 
-export { getBills }
+const addBill = async (bill) => {
+    return await API.post(`bills`, bill);
+}
+
+const updateBill = async (bill) => {
+    return await API.put(`bills/${bill.id}`, bill);
+}
+
+const deleteBill = async (billID) => {
+    return await API.delete(`bills/${billID}`);
+}
+
+const copyBillFromLastMonth = async () => {
+    return await API.post(`bills/fixed/copy-from-last-month`);
+}
+
+const payBill = async (billID) => {
+    return await API.post(`bills/${billID}/pay`);
+}
+
+export { getBills, getBillsByDate, addBill, updateBill, deleteBill, copyBillFromLastMonth, payBill }
